d3ec801dfd5ceb97c5c796c4ace90b98eec9a547
Thomas Körner <t.koerner@heimrich-hannot.de>
2020-06-22 17:11:43 +0200

    fixed hook

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,3 +4,5 @@ $GLOBALS['BE_MOD']['content']['tree'] = [
 ];
 
 $GLOBALS['TL_MODELS']['tl_tree'] = \HeimrichHannot\TreeBundle\Model\TreeModel::class;
+
+$GLOBALS['TL_HOOKS']['loadDataContainer']['huh_tree'] = [\HeimrichHannot\TreeBundle\EventListener\LoadDataContainerListener::class, '__invoke'];
