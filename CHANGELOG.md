1ac920aab8c234c5f2870cf3838735929d6e5759
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-08-17 16:25:28 +0200

    allow php 8, use contracts event class

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+All notable changes to this project will be documented in this file.
+
+## [Unreleased] - 2022-08-17
+- Changed: allow php 8
+- Fixed: symfony 5 compatibility
--- src/Event/BeforeRenderNodeEvent.php
+++ src/Event/BeforeRenderNodeEvent.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2020 Heimrich & Hannot GmbH
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
@@ -9,7 +9,7 @@
 namespace HeimrichHannot\TreeBundle\Event;
 
 use HeimrichHannot\TreeBundle\Model\TreeModel;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 class BeforeRenderNodeEvent extends Event
 {
